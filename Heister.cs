46ee34cd15680e2ab8af920d39dc60ebd0db1235
Adam <adam.e.sheaffer@gmail.com>
2019-11-19 10:13:59 -0600

    heist exercise and exception handling

---
+++ Heister.cs
@@ -0,0 +1,9 @@
+namespace HeistExercise
+{
+    public class Heister
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public decimal CourageFactor { get; set; }
+    }
+}
